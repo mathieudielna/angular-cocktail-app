2d47672d229a079f83b58c2c33c16949b7c56aac
Mathieu DIELNA <DIM@lusis.domain>
2022-09-23 16:31:55 +0200

    save state

--- src/app/shared/services/cocktail.service.ts
+++ src/app/shared/services/cocktail.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
+import { BehaviorSubject, tap, Observable } from 'rxjs';
 import { Cocktail } from '../interfaces/cocktail.interface';
 
 
@@ -10,69 +10,12 @@ import { Cocktail } from '../interfaces/cocktail.interface';
 export class CocktailService {
 
   constructor(private http : HttpClient) { 
-    this.seed();
+    //init cocktails
+    //this.seed();
   }
 
-  public cocktails$: BehaviorSubject<any> = new BehaviorSubject([
-    {
-      name: "Mojito",
-      img: "https://www.offcourses.net/9466-large_default/boisson-gazeuse-4-agrumes.jpg",
-      description: "The Mojito complimenting summer perfectly with a fresh minty taste. The mixture of white rum, mint, lime juice, sugar and soda water is crisp and clean with a relatively low alcohol content, the soda water can be replaced with sprite or 7-up. When preparing a mojito always crush the mint leaves as opposed to dicing to unlock oils that will assist with enhancing the minty flavour.",
-      ingredients : [
-        {
-        name : 'Rhum',
-        quantity : 0.25,
-        unite : 'L'
-      },
-        {
-        name : 'Citron',
-        quantity : 0.25,
-        unite : 'unit'
-      },
-        {
-        name : 'Orange',
-        quantity : 0.5,
-        unite : 'unit'
-      },
-    ],
-    },
-    {
-      name: "Cosmopolitan",
-      img: "https://wordpress.potagercity.fr/wp-content/uploads/2019/06/Boisson-fra%C3%AEche-m%C3%BBre-citron.jpg",
-      description: "The tangy concoction of vodka, triple sec, lime juice and cranberry juice has managed to leapfrog the venerable screwdriver as many vodka drinkers prefer the Cosmopolitan’s cleaner and slightly tart taste. The keys to the preparation of a Cosmopolitan are a good brand of cranberry juice and Cointreau Triple Sec, two essential elements to the drink.",
-      ingredients : [
-        {
-          name : 'Vodka',
-          quantity : 0.25,
-          unite : 'L'
-        },
-          {
-          name : 'Citron',
-          quantity : 0.25,
-          unite : 'unit'
-        },
-      ],
-    },
-    {
-      name: "Mai Tai",
-      img: "https://wordpress.potagercity.fr/wp-content/uploads/2019/06/Boisson-fra%C3%AEche-m%C3%BBre-citron.jpg",
-      description: "The Mai Tai is a Polynesian-style cocktail that has a fruity tropical taste sweet and vibrant. The mixture of light and dark rum, orange curacao, orgeat syrup and lime juice has been a symbol of Tahitian culture ever since the drink was first created.",
-      ingredients : [
-        {
-          name : 'Vodka',
-          quantity : 0.25,
-          unite : 'L'
-        },
-          {
-          name : 'Coca',
-          quantity : 0.55,
-          unite : 'cl'
-        },
-      ],
-    }
-  ]);
-
-  public selectedCocktail$: BehaviorSubject<Cocktail> = new BehaviorSubject(this.cocktails$.value[0]);
+  public cocktails$: BehaviorSubject<any> = new BehaviorSubject(null);
+  public selectedCocktail$: BehaviorSubject<any> = new BehaviorSubject(this.cocktails$.value[0]);
 
   public selectCocktail(index: number): void {
     this.selectedCocktail$.next(this.cocktails$.value[index]);
@@ -99,7 +42,15 @@ export class CocktailService {
     }))
   }
 
-  // init API
+  public fetchCocktails(): Observable<any> {
+    return this.http.get('http://www.restapi.fr/api/cocktails').pipe(
+      tap((cocktails) => {
+        this.cocktails$.next(cocktails);
+      })
+    )
+  }
+
+  // init API  
   // public seed() {
   //   this.http.post('https://www.restapi.fr/api/cocktails',[
   //   {
